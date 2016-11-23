db8dc6ded95f2a98107d049c579318b15b071226
Long N. Le <longle1@illinois.edu>
2016-11-23 16:07:22 -0600

    improve Suitron visualization of predicted data

--- suiron/core/SuironVZ.py
+++ suiron/core/SuironVZ.py
@@ -29,19 +29,20 @@ def visualize_data(filename, width=72, height=48, depth=3, cnn_model=None):
         # Extra debugging info (e.g. steering etc)
         cv2.putText(cur_img_array, "frame: %s" % str(i), (5,35), cv2.FONT_HERSHEY_SIMPLEX, 1, 255)
         cv2.putText(cur_img_array, "servo: %s" % str(cur_throttle), (5,70), cv2.FONT_HERSHEY_SIMPLEX, 1, 255)
-        cv2.putText(cur_img_array, "motor: %s" % str(cur_motor), (5,105), cv2.FONT_HERSHEY_SIMPLEX, 1, 255)
+        cv2.putText(cur_img_array, "motor: %s" % str(cur_motor), (5,100), cv2.FONT_HERSHEY_SIMPLEX, 1, 255)
 
         # Steering values
         cv2.line(cur_img_array, (240, 300), (240+cur_throttle, 200), (0, 255, 0), 3)
 
         # Motor values, RGB
-        cv2.line(cur_img_array, (50, 300), (50, 300-cur_motor), raw_motor_to_rgb(cur_motor), 3)
+        #cv2.line(cur_img_array, (50, 300), (50, 300-cur_motor), raw_motor_to_rgb(cur_motor), 3)
+        cv2.line(cur_img_array, (50, 300), (50, 300-cur_motor), (0, 255, 0), 3)
 
         # If we wanna visualize our cnn_model
         if cnn_model:
             y = cnn_model.predict([y_input])
-            servo_out = cnn_to_raw(y[0])         
-            cv2.line(cur_img_array, (240, 300), (240+int(servo_out), 200), (0, 0, 255), 3)
+            servo_out = int(cnn_to_raw(y[0]))         
+            cv2.line(cur_img_array, (250, 300), (250+servo_out, 200), (0, 0, 255), 3)
 
             # Can determine the motor our with a simple exponential equation
             # x = abs(servo_out-90)
@@ -50,8 +51,11 @@ def visualize_data(filename, width=72, height=48, depth=3, cnn_model=None):
             x_ = abs(servo_out - 90)
             motor_out = (7.64*np.e**(-0.096*x_)) - 1
             motor_out = int(80 - motor_out) # Only wanna go forwards
-            cv2.line(cur_img_array, (100, 160), (100, 160-(90-motor_out)), raw_motor_to_rgb(motor_out), 3)
-            print(motor_out, cur_motor)
+            cv2.line(cur_img_array, (60, 300), (60, 300-motor_out), (0, 0, 255), 3)
+            #print(motor_out, cur_motor)
+
+            cv2.putText(cur_img_array, "pServo: %s" % str(servo_out), (245,70), cv2.FONT_HERSHEY_SIMPLEX, 1, 255)
+            cv2.putText(cur_img_array, "pMotor: %s" % str(motor_out), (245,100), cv2.FONT_HERSHEY_SIMPLEX, 1, 255)
 
         # Show frame
         # Convert to BGR cause thats how OpenCV likes it
