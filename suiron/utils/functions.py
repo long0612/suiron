742ef72562621401654608331dfa87288c71e9ea
Long N. Le <longle1@illinois.edu>
2016-11-29 10:34:16 -0600

    fixed min_arduino not = 0

--- suiron/utils/functions.py
+++ suiron/utils/functions.py
@@ -6,7 +6,7 @@ def arduino_map(x, in_min, in_max, out_min, out_max):
 
 # Converts raw values to target (Y) values
 # for the convolutional neural network
-def raw_to_cnn(y, min_arduino=0.0, max_arduino=150.0):
+def raw_to_cnn(y, min_arduino=-150.0, max_arduino=150.0):
     # Map from R to -1,1
     # Conversion because output activation is tanh
     y_ = arduino_map(y, min_arduino, max_arduino, -1.0, 1.0)
@@ -14,7 +14,7 @@ def raw_to_cnn(y, min_arduino=0.0, max_arduino=150.0):
 
 # Converts convolutional neural network outputs 
 # to raw outputs
-def cnn_to_raw(y, min_arduino=0.0, max_arduino=150.0):
+def cnn_to_raw(y, min_arduino=-150.0, max_arduino=150.0):
     # Get highest index value and map
     # it back
     y_ = y[np.argmax(y)]
